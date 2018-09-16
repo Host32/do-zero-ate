d7c2bf593f8e5d21acbf0e15977773c0b69ff0d6
Ivan Miranda <ivan.i.n@hotmail.com>
2018-09-15 21:49:20 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+O ponto central da interface é o layout. A parte mais interessante é escolher bem a técnica usada para posicionar a imagem de capa, apostei num position absolute dentro de um bloco com position relative, por isso o layout precisa ser bem planejado para manter a consistência.
+
+![](https://i.imgur.com/yW5GuyU.png)
+
+Após planejar a técnica, resolvi usar Bootstrap mesmo porque já estou mais acostumado com ele. De resto bastou customizar os componentes de acordo com o guia visual
---
+++ js/index.js
@@ -0,0 +1,28 @@
+$(function () {
+  let bar = new ProgressBar.Circle('#rating', {
+    strokeWidth: 10,
+    easing: 'easeInOut',
+    duration: 1400,
+    color: '#e5bc23',
+    trailColor: '#4a4a4a',
+    trailWidth: 10,
+    svgStyle: {
+      width: '4em'
+    },
+    text: {
+      value: '7.0',
+      style: {
+        color: '#fff',
+        'font-weight': 'bold',
+        'font-size': '1.5rem',
+        position: 'absolute',
+        left: '45%',
+        top: '20%',
+        padding: 0,
+        margin: 0,
+      }
+    }
+  });
+
+  bar.animate(0.7);
+});
